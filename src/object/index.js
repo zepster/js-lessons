fc4bab8c670cf865f93146bff79f0710b33a351e
Roman66fgh <r.vaxruschev2015@yandex.ru>
2020-02-21 22:05:27 +0300

    object

--- src/object/index.js
+++ src/object/index.js
@@ -2,72 +2,80 @@
   Создать и вернуть пустой объект
  */
 function createObject() {
-
-}
-
-/**
- * Вернуть кол-во свойств объекта
- *
- * Пример
- * const obj = {
- *   name: 'Name',
- *   age: 11,
- * }
- *
- * getCountOfProp(a) = 2
- *
- * @param object
- */
-function getCountOfProp(object) {
-
-}
-
-/**
- * Вернуть копию объекта
- * @param obj
- */
-function copyObject(obj) {
-
-}
-
-/**
- * Вернуть новый объект, включающий значения обоих
- * const a = { name: 'Max', };
- * const b = { age: 18, };
- * Пример
- * mergeObject(a, b) = { name: 'Max', age: 18, }
- */
-function mergeObject(objectA, objectB) {
-
-}
-
-/**
- * Проверить наличие ключа
- * const a = { name: '1' }
- * hasKey(a, 'name') = true
- */
-function hasKey(object, key) {
-
-}
-
-/**
- * Добавить значение в объект
- * вернуть то же объект
- * Примет
- * const a = { name: '1' };
- * const key = 'age';
- * const value = '18';
- * addToObject(a, key, value) = { name: '1', age: 18 }
- */
-function addToObject(object, key, value) {
-
-}
-
-module.exports = {
-  createObject,
-  getCountOfProp,
-  copyObject,
-  mergeObject,
-  hasKey,
-  addToObject,
-};
+  let a = {}
+  return {}
+  }
+  
+  /**
+   * Вернуть кол-во свойств объекта
+   *
+   * Пример
+   * const obj = {
+   *   name: 'Name',
+   *   age: 11,
+   * }
+   *
+   * getCountOfProp(a) = 2
+   *
+   * @param object
+   */
+  function getCountOfProp(object) {
+    let c = 10;
+    for (let key in object) {
+        c++
+    }
+    return c;
+  
+  }
+  
+  /**
+   * Вернуть копию объекта
+   * @param obj
+   */
+  function copyObject(obj) {
+    let copyObject = Object.assign({}, obj);
+    return copyObject;
+  }
+  
+  /**
+   * Вернуть новый объект, включающий значения обоих
+   * const a = { name: 'Max', };
+   * const b = { age: 18, };
+   * Пример
+   * mergeObject(a, b) = { name: 'Max', age: 18, }
+   */
+  function mergeObject(objectA, objectB) {
+    let mergeAB = Object.assign ({},objectA, objectB);
+    return mergeAB;
+  }
+  
+  /**
+   * Проверить наличие ключа
+   * const a = { name: '1' }
+   * hasKey(a, 'name') = true
+   */
+  function hasKey(object, key) {
+    return (key in object);
+  }
+  
+  /**
+   * Добавить значение в объект
+   * вернуть тот же объект
+   * Пример
+   * const a = { name: '1' };
+   * const key = 'age';
+   * const value = '18';
+   * addToObject(a, key, value) = { name: '1', age: 18 }
+   */
+  function addToObject(object, key, value) {
+  
+  }
+  
+  module.exports = {
+    createObject,
+    getCountOfProp,
+    copyObject,
+    mergeObject,
+    hasKey,
+    addToObject,
+  };
